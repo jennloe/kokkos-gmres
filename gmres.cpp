aab86b67e828dcf9676266cade097c7f642a6a47
Jennifer Loe <jloe@sandia.gov>
2021-06-07 16:22:16 -0600

    More cleanup: Removed extra and unused variables.

--- gmres.cpp
+++ gmres.cpp
@@ -28,16 +28,6 @@ int main(int argc, char *argv[]) {
   double nrmB; 
   double relRes, shortRelRes;
   
-  //EXAMPLE: Parse cmnd line args: 
-    /*for (int i=1;i<argc;++i) {
-      const std::string& token = argv[i];
-      if (token == std::string("-N")) N = std::atoi(argv[++i]);
-      if (token == std::string("-B")) Blk = std::atoi(argv[++i]);
-      if (token == std::string("-Task")) Task = std::atoi(argv[++i]);
-      if (token == std::string("-TeamSize")) TeamSize = std::atoi(argv[++i]);
-    }   
-    printf(" :::: Testing (N = %d, Blk = %d, TeamSize = %d (0 is AUTO))\n", N, Blk, TeamSize); */ 
-  
   for (int i=1;i<argc;++i) {
     const std::string& token = argv[i];
     if (token == std::string("--filename")) filename = argv[++i];
@@ -71,24 +61,19 @@ int main(int argc, char *argv[]) {
   ViewVectorType B(Kokkos::ViewAllocateWithoutInitializing("B"),n);//right-hand side vec
   ViewVectorType Res(Kokkos::ViewAllocateWithoutInitializing("Res"),n); //Residual vector
   ViewVectorType Wj(Kokkos::ViewAllocateWithoutInitializing("W_j"),n); //Tmp work vector 1
-  ViewVectorType TmpVec(Kokkos::ViewAllocateWithoutInitializing("TmpVec"),n); //Tmp work vector 2
-  ViewVectorType LsVec("LsVec",m+1); //Small rhs of least-squares problem
-  ViewVectorType::HostMirror LsVec_h = Kokkos::create_mirror_view(LsVec);
-  ViewVectorType::HostMirror GVec_h = Kokkos::create_mirror_view(LsVec); //Copy of this for Givens Rotation intermediate solve. 
-  ViewVectorType::HostMirror TmpGVec_h = Kokkos::create_mirror_view(LsVec); //Copy of this for Givens Rotation intermediate solve. 
-  ViewMatrixType LsSoln("LsSoln",m,1);
-  ViewMatrixType::HostMirror LsSoln_h = Kokkos::create_mirror_view(LsSoln);//Needed for debugging only
-  ViewMatrixType GLsSoln("GLsSoln",m,1);//LS solution vec for Givens Rotation //TODO can make this 1-D view??
+  ViewVectorType TmpVec(Kokkos::ViewAllocateWithoutInitializing("TmpVec"),n); //Tmp work vector 2 //TODO is this needed?
+  ViewHostVectorType GVec_h("GVec",m+1);
+  ViewMatrixType GLsSoln("GLsSoln",m,1);//LS solution vec for Givens Rotation. Must be 2-D for trsm. 
   ViewMatrixType::HostMirror GLsSoln_h = Kokkos::create_mirror_view(GLsSoln); //This one is needed for triangular solve. 
   ViewHostVectorType CosVal_h("CosVal",m);
   ViewHostVectorType SinVal_h("SinVal",m);
-
-  ViewMatrixType Q("Q",m+1,m); //Q matrix for QR factorization of H
-  ViewMatrixType::HostMirror H_h = Kokkos::create_mirror_view(Q); //Make H into a host view of Q. 
-  ViewMatrixType::HostMirror H_copy_h = Kokkos::create_mirror_view(Q); // Copy of H to transform with Givens Rotations.
-  ViewMatrixType RFactor("RFactor",m,m);// Triangular matrix for QR factorization of H
   ViewMatrixType V(Kokkos::ViewAllocateWithoutInitializing("V"),n,m+1);
-  ViewMatrixType VSub = Kokkos::subview(V,Kokkos::ALL,Kokkos::make_pair(0,m)); //Subview of 1st m cols for updating soln.
+  ViewMatrixType VSub = Kokkos::subview(V,Kokkos::ALL,Kokkos::make_pair(0,m)); //Subview of 1st m cols for updating soln. 
+  //TODO why initialize VSub?  Changes later. 
+
+  ViewMatrixType Q("Q",m+1,m); //Q matrix for QR factorization of H //Only used in Arn Rec debug. 
+  ViewMatrixType::HostMirror H_h = Kokkos::create_mirror_view(Q); //Make H into a host view of Q. 
+  ViewMatrixType RFactor("RFactor",m,m);// Triangular matrix for QR factorization of H
 
 
   // Make rhs random.
@@ -109,23 +94,20 @@ int main(int argc, char *argv[]) {
   std::cout << "Initial trueRes is : " << trueRes << std::endl;
     
   while( !converged && cycle < cycLim){
-    LsVec_h(0) = trueRes;
-    Kokkos::deep_copy(LsVec, LsVec_h);
     GVec_h(0) = trueRes;
 
     // Run Arnoldi iteration:
-    auto V0 = Kokkos::subview(V,Kokkos::ALL,0);
-    Kokkos::deep_copy(V0,Res);
-    KokkosBlas::scal(V0,1.0/trueRes,V0); //V0 = V0/norm(V0)
+    auto Vj = Kokkos::subview(V,Kokkos::ALL,0);
+    Kokkos::deep_copy(Vj,Res);
+    KokkosBlas::scal(Vj,1.0/trueRes,Vj); //V0 = V0/norm(V0)
 
     for (int j = 0; j < m; j++){
-      auto Vj = Kokkos::subview(V,Kokkos::ALL,j); //TODO Could skip this one and use the v0 earlier and vj at end??
+      //auto Vj = Kokkos::subview(V,Kokkos::ALL,j); //TODO Could skip this one and use the v0 earlier and vj at end??
       KokkosSparse::spmv("N", 1.0, A, Vj, 0.0, Wj); //wj = A*Vj
       // Think this is MGS ortho, but 1 vector at a time?
       for (int i = 0; i <= j; i++){
         auto Vi = Kokkos::subview(V,Kokkos::ALL,i); 
         H_h(i,j) = KokkosBlas::dot(Vi,Wj);  //Vi^* Wj  //TODO is this the right order for cmplx dot product?
-        H_copy_h(i,j) = H_h(i,j);
         KokkosBlas::axpy(-H_h(i,j),Vi,Wj);//wj = wj-Hij*Vi //Host
       }
       //Re-orthog:
@@ -134,41 +116,38 @@ int main(int argc, char *argv[]) {
         tmpScalar = KokkosBlas::dot(Vi,Wj); //Vi^* Wj
         KokkosBlas::axpy(-tmpScalar,Vi,Wj);//wj = wj-tmpScalar*Vi
         H_h(i,j) = H_h(i,j) + tmpScalar; 
-        H_copy_h(i,j) = H_h(i,j);
         KokkosBlas::scal(TmpVec,H_h(i,j),Vi);//TmpVec = H_h(i,j)*Vi 
       }*/
       
       H_h(j+1,j) = KokkosBlas::nrm2(Wj); 
-      H_copy_h(j+1,j) = H_h(j+1,j);
       if(H_h(j+1,j) < 1e-14){ //Host
         throw std::runtime_error("Lucky breakdown"); //TODO deal with this correctly? Did we check for convergence?
       }
 
+      Vj = Kokkos::subview(V,Kokkos::ALL,j+1); 
+      KokkosBlas::scal(Vj,1.0/H_h(j+1,j),Wj); // Wj = Vj/H(j+1,j)
+
       //Apply Givens rotation and compute shortcut residual:
       for(int i=0; i<j; i++){
-        ST tempVal = CosVal_h(i)*H_copy_h(i,j) + SinVal_h(i)*H_copy_h(i+1,j);
-        H_copy_h(i+1,j) = -SinVal_h(i)*H_copy_h(i,j) + CosVal_h(i)*H_copy_h(i+1,j);
-        H_copy_h(i,j) = tempVal;
+        ST tempVal = CosVal_h(i)*H_h(i,j) + SinVal_h(i)*H_h(i+1,j);
+        H_h(i+1,j) = -SinVal_h(i)*H_h(i,j) + CosVal_h(i)*H_h(i+1,j);
+        H_h(i,j) = tempVal;
       }
-      ST h1 = H_copy_h(j,j);
-      ST h2 = H_copy_h(j+1,j);
+      ST h1 = H_h(j,j);
+      ST h2 = H_h(j+1,j);
       ST mod = (sqrt(h1*h1 + h2*h2));
       CosVal_h(j) = h1/mod;
       SinVal_h(j) = h2/mod;
 
       //Have to apply this Givens rotation outside the loop- requires the values adjusted in loop to compute cos and sin
-      H_copy_h(j,j) = CosVal_h(j)*H_copy_h(j,j) + SinVal_h(j)*H_copy_h(j+1,j);
-      H_copy_h(j+1,j) = 0.0; //Do this outside of loop so we get an exact zero here. 
+      H_h(j,j) = CosVal_h(j)*H_h(j,j) + SinVal_h(j)*H_h(j+1,j);
+      H_h(j+1,j) = 0.0; //Do this outside of loop so we get an exact zero here. 
 
       GVec_h(j+1) = GVec_h(j)*(-SinVal_h(j));
       GVec_h(j) = GVec_h(j)*CosVal_h(j);
       shortRelRes = abs(GVec_h(j+1))/nrmB;
 
-      std::cout << std::endl;
       std::cout << "Shortcut relative residual for iteration " << j+(cycle*m) << " is: " << shortRelRes << std::endl;
-
-      Vj = Kokkos::subview(V,Kokkos::ALL,j+1); 
-      KokkosBlas::scal(Vj,1.0/H_h(j+1,j),Wj); // Wj = Vj/H(j+1,j)
 
       //If short residual converged, or time to restart, check true residual
       if( shortRelRes < convTol || j == m-1 ) {
@@ -177,8 +156,8 @@ int main(int argc, char *argv[]) {
         auto GVecSub_h = Kokkos::subview(GVec_h, Kokkos::make_pair(0,m));
         Kokkos::deep_copy(GLsSolnSub_h, GVecSub_h); //Copy LS rhs vec for triangle solve.
         auto GLsSolnSub2_h = Kokkos::subview(GLsSoln_h,Kokkos::make_pair(0,j+1),Kokkos::ALL);
-        auto H_copySub_h = Kokkos::subview(H_copy_h, Kokkos::make_pair(0,j+1), Kokkos::make_pair(0,j+1)); //TODO could change type from auto? 
-        KokkosBlas::trsm("L", "U", "N", "N", 1.0, H_copySub_h, GLsSolnSub2_h); //GLsSoln = H\GLsSoln
+        auto H_Sub_h = Kokkos::subview(H_h, Kokkos::make_pair(0,j+1), Kokkos::make_pair(0,j+1)); //TODO could change type from auto? 
+        KokkosBlas::trsm("L", "U", "N", "N", 1.0, H_Sub_h, GLsSolnSub2_h); //GLsSoln = H\GLsSoln
         Kokkos::deep_copy(GLsSoln, GLsSoln_h);
 
         //Update solution and compute residual with Givens:
@@ -237,14 +216,12 @@ int main(int argc, char *argv[]) {
     for (int i1 = 0; i1 < m; i1++){ std::cout << nrmARec_h(i1) << " " ; }
     std::cout << std::endl; */
     
-    //Zero out Givens rotation vector. 
+    //Zero out Givens rotation vector and H matrix. 
     Kokkos::deep_copy(GVec_h,0);
+    Kokkos::deep_copy(H_h,0); //TODO is this step really needed?  
 
-    //TODO Can probably remove this at the end.  Used so that we can run full LS problem to 
-    //check short residual at each iteration.
-    Kokkos::deep_copy(H_h,0);
-    Kokkos::deep_copy(H_copy_h,0);
     cycle++;
+
     //This is the end, or it's time to restart. Update solution to most recent vector.
     Kokkos::deep_copy(X, Xiter);
   }

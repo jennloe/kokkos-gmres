0e7d83d32d4e7589526a12bdc53b237a6d9a9046
Jennifer Loe <jloe@sandia.gov>
2021-06-21 10:06:06 -0600

    It builds with ST=complex now, but the math is wrong somewhere.

--- gmres.hpp
+++ gmres.hpp
@@ -10,19 +10,26 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
   //TODO: Check- don't pass views by ref, right? What abt CRS matrix?
   // But Kokkos Blas does pass by ref.... 
   void gmres( KokkosSparse::CrsMatrix<ScalarType, OrdinalType, EXSP> A, Kokkos::View<ScalarType*, Layout, EXSP> B,
-        Kokkos::View<ScalarType*, Layout, EXSP> X, ScalarType tol = 1e-8, int m=50, int maxRestart=50, std::string ortho = "CGS"){
+        Kokkos::View<ScalarType*, Layout, EXSP> X, typename Kokkos::Details::ArithTraits<ScalarType>::mag_type tol = 1e-8, int m=50, int maxRestart=50, std::string ortho = "CGS"){
+
+  typedef Kokkos::Details::ArithTraits<ScalarType> ArithTraits;
+  typedef typename ArithTraits::val_type ST; // So this code will run with ScalarType = std::complex<T>.
+  typedef typename ArithTraits::mag_type MT; 
+  ST one = ArithTraits::one();
+  ST zero = ArithTraits::zero();
 
   //TODO: Should these really be layout left?
-  typedef Kokkos::View<ScalarType*,Layout, EXSP> ViewVectorType;
+  typedef Kokkos::View<ST*,Layout, EXSP> ViewVectorType;
   //TODO: Should these be Layout left or templated layou?  Think mostly used internally.  
-  typedef Kokkos::View<ScalarType*,Kokkos::LayoutLeft, Kokkos::HostSpace> ViewHostVectorType; 
-  typedef Kokkos::View<ScalarType**,Kokkos::LayoutLeft, EXSP> ViewMatrixType; 
+  typedef Kokkos::View<ST*,Kokkos::LayoutLeft, Kokkos::HostSpace> ViewHostVectorType; 
+  typedef Kokkos::View<ST**,Kokkos::LayoutLeft, EXSP> ViewMatrixType; 
 
   bool converged = false;
   int cycle = 0;
   int numIters;  //Number of iterations within the cycle before convergence.
-  double trueRes; //Keep this in double regardless so we know how small error gets.
-  double nrmB, relRes, shortRelRes;
+  MT trueRes; //Keep this in double regardless so we know how small error gets. //TODO: Should this be in double?
+  // We are not mixing precisions.  So maybe it should be scalarType? or MT?
+  MT nrmB, relRes, shortRelRes;
   
   std::cout << "Convergence tolerance is: " << tol << std::endl;
 
@@ -30,7 +37,7 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
   ViewVectorType Xiter("Xiter",n); //Intermediate solution at iterations before restart. 
   ViewVectorType Res(Kokkos::ViewAllocateWithoutInitializing("Res"),n); //Residual vector
   ViewVectorType Wj(Kokkos::ViewAllocateWithoutInitializing("W_j"),n); //Tmp work vector 1
-  ViewHostVectorType GVec_h("GVec",m+1);
+  ViewHostVectorType GVec_h(Kokkos::ViewAllocateWithoutInitializing("GVec"),m+1);
   ViewMatrixType GLsSoln("GLsSoln",m,1);//LS solution vec for Givens Rotation. Must be 2-D for trsm. 
   typename ViewMatrixType::HostMirror GLsSoln_h = Kokkos::create_mirror_view(GLsSoln); //This one is needed for triangular solve. 
   ViewHostVectorType CosVal_h("CosVal",m);
@@ -45,8 +52,8 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
   //Compute initial residuals:
   nrmB = KokkosBlas::nrm2(B);
   Kokkos::deep_copy(Res,B);
-  KokkosSparse::spmv("N", 1.0, A, X, 0.0, Wj); // wj = Ax
-  KokkosBlas::axpy(-1.0, Wj, Res); // res = res-Wj = b-Ax. 
+  KokkosSparse::spmv("N", one, A, X, zero, Wj); // wj = Ax
+  KokkosBlas::axpy(-one, Wj, Res); // res = res-Wj = b-Ax. 
   trueRes = KokkosBlas::nrm2(Res);
   relRes = trueRes/nrmB;
   std::cout << "Initial trueRes is : " << trueRes << std::endl;
@@ -57,10 +64,10 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
     // Run Arnoldi iteration:
     auto Vj = Kokkos::subview(V,Kokkos::ALL,0); //TODO:pre-declare this so no auto?
     Kokkos::deep_copy(Vj,Res);
-    KokkosBlas::scal(Vj,1.0/trueRes,Vj); //V0 = V0/norm(V0)
+    KokkosBlas::scal(Vj,one/trueRes,Vj); //V0 = V0/norm(V0)
 
     for (int j = 0; j < m; j++){
-      KokkosSparse::spmv("N", 1.0, A, Vj, 0.0, Wj); //wj = A*Vj
+      KokkosSparse::spmv("N", one, A, Vj, zero, Wj); //wj = A*Vj
       if( ortho == "MGS"){
         for (int i = 0; i <= j; i++){
           auto Vi = Kokkos::subview(V,Kokkos::ALL,i); 
@@ -73,14 +80,14 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
         auto V0j = Kokkos::subview(V,Kokkos::ALL,Kokkos::make_pair(0,j+1)); //TODO:pre-declare this so no auto?
         auto Hj = Kokkos::subview(H,Kokkos::make_pair(0,j+1) ,j);
         auto Hj_h = Kokkos::subview(H_h,Kokkos::make_pair(0,j+1) ,j);
-        KokkosBlas::gemv("T", 1.0, V0j, Wj, 0.0, Hj); // Hj = Vj^T * wj
-        KokkosBlas::gemv("N", -1.0, V0j, Hj, 1.0, Wj); // wj = wj - Vj * Hj
+        KokkosBlas::gemv("C", one, V0j, Wj, zero, Hj); // Hj = Vj^T * wj
+        KokkosBlas::gemv("N", -one, V0j, Hj, one, Wj); // wj = wj - Vj * Hj
 
         //Re-orthog CGS:
         ViewVectorType tmp(Kokkos::ViewAllocateWithoutInitializing("tmp"),j+1); 
-        KokkosBlas::gemv("T", 1.0, V0j, Wj, 0.0, tmp); // tmp (Hj) = Vj^T * wj
-        KokkosBlas::gemv("N", -1.0, V0j, tmp, 1.0, Wj); // wj = wj - Vj * tmp 
-        KokkosBlas::axpy(1.0, tmp, Hj); // Hj = Hj + tmp
+        KokkosBlas::gemv("C", one, V0j, Wj, zero, tmp); // tmp (Hj) = Vj^T * wj
+        KokkosBlas::gemv("N", -one, V0j, tmp, one, Wj); // wj = wj - Vj * tmp 
+        KokkosBlas::axpy(one, tmp, Hj); // Hj = Hj + tmp
         Kokkos::deep_copy(Hj_h,Hj);
       }
       else {
@@ -95,29 +102,30 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
         H_h(i,j) = H_h(i,j) + tmpScalar; 
       }*/
       
-      H_h(j+1,j) = KokkosBlas::nrm2(Wj); 
-      if(H_h(j+1,j) < 1e-14){ //Host
+      MT tmpNrm = KokkosBlas::nrm2(Wj);
+      H_h(j+1,j) = tmpNrm; 
+      if(tmpNrm < 1e-14){ //Host
         throw std::runtime_error("Lucky breakdown"); //TODO deal with this correctly? Did we check for convergence?
       }
 
       Vj = Kokkos::subview(V,Kokkos::ALL,j+1); 
-      KokkosBlas::scal(Vj,1.0/H_h(j+1,j),Wj); // Wj = Vj/H(j+1,j)
+      KokkosBlas::scal(Vj,one/H_h(j+1,j),Wj); // Wj = Vj/H(j+1,j)
 
       //Apply Givens rotation and compute shortcut residual:
       for(int i=0; i<j; i++){
-        ScalarType tempVal = CosVal_h(i)*H_h(i,j) + SinVal_h(i)*H_h(i+1,j);
+        ST tempVal = CosVal_h(i)*H_h(i,j) + SinVal_h(i)*H_h(i+1,j);
         H_h(i+1,j) = -SinVal_h(i)*H_h(i,j) + CosVal_h(i)*H_h(i+1,j);
         H_h(i,j) = tempVal;
       }
-      ScalarType h1 = H_h(j,j);
-      ScalarType h2 = H_h(j+1,j);
-      ScalarType mod = (sqrt(h1*h1 + h2*h2));
+      ST h1 = H_h(j,j);
+      ST h2 = H_h(j+1,j);
+      ST mod = (sqrt(h1*h1 + h2*h2));
       CosVal_h(j) = h1/mod;
       SinVal_h(j) = h2/mod;
 
       //Have to apply this Givens rotation outside the loop- requires the values adjusted in loop to compute cos and sin
       H_h(j,j) = CosVal_h(j)*H_h(j,j) + SinVal_h(j)*H_h(j+1,j);
-      H_h(j+1,j) = 0.0; //Do this outside of loop so we get an exact zero here. 
+      H_h(j+1,j) = zero; //Do this outside of loop so we get an exact zero here. 
 
       GVec_h(j+1) = GVec_h(j)*(-SinVal_h(j));
       GVec_h(j) = GVec_h(j)*CosVal_h(j);
@@ -133,17 +141,17 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
         Kokkos::deep_copy(GLsSolnSub_h, GVecSub_h); //Copy LS rhs vec for triangle solve.
         auto GLsSolnSub2_h = Kokkos::subview(GLsSoln_h,Kokkos::make_pair(0,j+1),Kokkos::ALL);
         auto H_Sub_h = Kokkos::subview(H_h, Kokkos::make_pair(0,j+1), Kokkos::make_pair(0,j+1)); //TODO could change type from auto? 
-        KokkosBlas::trsm("L", "U", "N", "N", 1.0, H_Sub_h, GLsSolnSub2_h); //GLsSoln = H\GLsSoln
+        KokkosBlas::trsm("L", "U", "N", "N", one, H_Sub_h, GLsSolnSub2_h); //GLsSoln = H\GLsSoln
         Kokkos::deep_copy(GLsSoln, GLsSoln_h);
 
         //Update solution and compute residual with Givens:
         VSub = Kokkos::subview(V,Kokkos::ALL,Kokkos::make_pair(0,j+1)); 
         Kokkos::deep_copy(Xiter,X); //Can't overwrite X with intermediate solution.
         auto GLsSolnSub3 = Kokkos::subview(GLsSoln,Kokkos::make_pair(0,j+1),0);
-        KokkosBlas::gemv ("N", 1.0, VSub, GLsSolnSub3, 1.0, Xiter); //x_iter = x + V(1:j+1)*lsSoln
-        KokkosSparse::spmv("N", 1.0, A, Xiter, 0.0, Wj); // wj = Ax
+        KokkosBlas::gemv ("N", one, VSub, GLsSolnSub3, one, Xiter); //x_iter = x + V(1:j+1)*lsSoln
+        KokkosSparse::spmv("N", one, A, Xiter, zero, Wj); // wj = Ax
         Kokkos::deep_copy(Res,B); // Reset r=b.
-        KokkosBlas::axpy(-1.0, Wj, Res); // r = b-Ax. 
+        KokkosBlas::axpy(-one, Wj, Res); // r = b-Ax. 
         trueRes = KokkosBlas::nrm2(Res);
         relRes = trueRes/nrmB;
         std::cout << "True Givens relative residual for iteration " << j+(cycle*m) << " is : " << trueRes/nrmB << std::endl;
@@ -169,7 +177,7 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
 
     /*//DEBUG: Check orthogonality of V:
     ViewMatrixType Vsm("Vsm", m+1, m+1);
-      KokkosBlas::gemm("T","N", 1.0, V, V, 0.0, Vsm); // Vsm = V^T * V
+      KokkosBlas::gemm("C","N", one, V, V, zero, Vsm); // Vsm = V^T * V
       ViewVectorType nrmV("nrmV",m+1);
     KokkosBlas::nrm2(nrmV, Vsm); //nrmV = norm(Vsm)
     std::cout << "Norm of V^T V: " << std::endl;
@@ -181,9 +189,9 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
     Kokkos::deep_copy(H,H_h);
     ViewMatrixType AV("AV", n, m);
     ViewMatrixType VH("VH", n, m);
-    KokkosSparse::spmv("N", 1.0, A, VSub, 0.0, AV); //AV = A*V_m
-    KokkosBlas::gemm("N","N", 1.0, V, H, 0.0, VH); //VH = V*H
-    KokkosBlas::axpy(-1.0, AV, VH); //VH = VH-AV
+    KokkosSparse::spmv("N", one, A, VSub, zero, AV); //AV = A*V_m
+    KokkosBlas::gemm("N","N", one, V, H, zero, VH); //VH = V*H
+    KokkosBlas::axpy(-one, AV, VH); //VH = VH-AV
     ViewVectorType nrmARec("ARNrm", m);
     ViewVectorType::HostMirror nrmARec_h = Kokkos::create_mirror_view(nrmARec); 
     KokkosBlas::nrm2( nrmARec, VH); //nrmARec = norm(VH)
@@ -192,10 +200,6 @@ template< class ScalarType, class Layout, class EXSP, class OrdinalType = int >
     for (int i1 = 0; i1 < m; i1++){ std::cout << nrmARec_h(i1) << " " ; }
     std::cout << std::endl; */
     
-    //Zero out Givens rotation vector and H matrix. 
-    Kokkos::deep_copy(GVec_h,0);
-    Kokkos::deep_copy(H_h,0); //TODO is this step really needed?  
-
     cycle++;
 
     //This is the end, or it's time to restart. Update solution to most recent vector.
